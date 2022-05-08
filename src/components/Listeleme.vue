093922dd9dcc9363ac3ea4f611411541d2efc394
MCanerD <mahircanerdogan@hotmail.com>
2022-05-08 19:20:58 +0300

    1

---
+++ src/components/Listeleme.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="list-group" v-for="(eleman, index) in elemanlar" :key="eleman.id">
+    <li class="list-group-item">
+      <CardView :kardNo="index">
+        <p>{{ eleman.bilgiMetni }}</p>
+        <ul
+          class="list-group list-group-hor  zontal"
+          v-for="begenen in eleman.begeniler"
+          :key="begenen"
+        >
+          <li class="list-group-item">
+            {{ begenen.kAdi }}
+          </li>
+        </ul>
+      </CardView>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["elemanlar"],
+};
+</script>
+
+<style scoped>
+</style>
